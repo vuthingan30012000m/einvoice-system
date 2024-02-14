f8c232929eaeb311764acee808ad48dc0c1dca23
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-14 11:05:27 +0700

    update main.ts and main.ts

--- microservices/user-service/src/main.ts
+++ microservices/user-service/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // console.log('Users Microservice is Running!');
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: ['nats://nats'],
+        // servers: ['nats://nats'],
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
