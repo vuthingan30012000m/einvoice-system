b55f951d259e4478612ad9b18026573788300cc1
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-14 10:07:45 +0700

    update main.ts

--- microservices/api-gateway/src/main.ts
+++ microservices/api-gateway/src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+
+  // app.setGlobalPrefix('api/product');
+
+  const options = new DocumentBuilder().build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      disableErrorMessages: true,
+      whitelist: true,
+    }),
+  );
+
+  app.enableShutdownHooks();
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port, async () => {
+    logger.log(`Server is running on: ${await app.getUrl()}`);
+  });
 }
 bootstrap();
