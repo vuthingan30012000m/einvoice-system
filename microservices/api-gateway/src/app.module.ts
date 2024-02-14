9b6d7a982fda5a705f22af24100ca72a3c52a7ef
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-14 10:16:46 +0700

    build(deps): update docker-compose.yml, package-lock.json, package.json and app.module.ts

--- microservices/api-gateway/src/app.module.ts
+++ microservices/api-gateway/src/app.module.ts
@@ -1,7 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
-  imports: [],
+  imports: [ ConfigModule.forRoot({
+    isGlobal: true,
+    validationSchema: Joi.object({
+      NATS_SERVICE: Joi.string().required(),
+      DATABASE_HOST: Joi.string().required(),
+      DATABASE_PORT: Joi.string().required(),
+      DATABASE_USERNAME: Joi.string().required(),
+      DATABASE_PASSWORD: Joi.string().required(),
+      DATABASE_NAME: Joi.string().required(),
+    }),
+  }),
+    ClientsModule.register([
+      {
+        name: 'NATS_SERVICE',
+        transport: Transport.NATS,
+        options: {
+          servers: [process.env.NATS_SERVICE || 'nats://nats'],
+        },
+      },
+    ]),
+  ],
   controllers: [],
   providers: [],
 })
