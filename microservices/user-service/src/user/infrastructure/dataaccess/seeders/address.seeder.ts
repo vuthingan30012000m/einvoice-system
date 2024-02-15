92fcdc42b8c277474ec5d8a742b39d386766751f
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-15 18:35:36 +0700

    delete 1 file and update 1 file

--- microservices/user-service/src/user/infrastructure/dataaccess/seeders/address.seeder.ts
+++ microservices/user-service/src/user/infrastructure/dataaccess/seeders/address.seeder.ts
@@ -57,54 +57,57 @@ export class AddressSeeder implements OnModuleInit {
       const dataDistrict: {
         id: number;
         name: string;
+        city:number;
       }[] = JSON.parse(rawDistrictData);
 
-      // for (const item of dataDistrict) {
-      //   const existingDistrict = await this.districtRepository.findOneBy({
-      //     id: item.id,
-      //   });
+      for (const item of dataDistrict) {
+      console.log("🚀 ~ AddressSeeder ~ onModuleInit ~ item:", item)
+        const existingDistrict = await this.districtRepository.findOneBy({
+          id: item.id,
+        });
 
-      //   if (existingDistrict) {
-      //     await this.districtRepository.update(existingDistrict.id, {
-      //       name: item.name,
-      //     });
-      //   } else {
-      //     const newDistrict = this.districtRepository.create({
-      //       id: item.id,
-      //       name: item.name,
-      //     });
+        if (existingDistrict) {
+          await this.districtRepository.update(existingDistrict.id, {
+            name: item.name,
+          });
+        } else {
+          const newDistrict = this.districtRepository.create({
+            id: item.id,
+            name: item.name,
+          });
 
-      //     await this.districtRepository.save(newDistrict);
-      //   }
-      // }
+          await this.districtRepository.save(newDistrict);
+        }
+      }
 
-      // var jsonFilePath = path.join(__dirname, 'address.ward.seeder.json');
-      // var rawData = fs.readFileSync(jsonFilePath, 'utf8');
+      const jsonFileWardPath = path.join(__dirname, 'address.ward.seeder.json');
+      const rawWardData = fs.readFileSync(jsonFileWardPath, 'utf8');
 
-      // var data: {
-      //   id: number;
-      //   name: string;
-      // }[] = JSON.parse(rawData);
+      const dataWard: {
+        id: number;
+        name: string;
+        city:number;
+      }[] = JSON.parse(rawWardData);
 
-      // for (const item of data) {
-      //   const existingWard = await this.wardRepository.findOneBy({
-      //     id: item.id,
-      //   });
+      for (const item of dataWard) {
+      console.log("🚀 ~ AddressSeeder ~ onModuleInit ~ item:", item)
+        const existingWard = await this.wardRepository.findOneBy({
+          id: item.id,
+        });
 
-      //   if (existingWard) {
-      //     await this.wardRepository.update(existingWard.id, {
-      //       name: item.name,
-      //     });
-      //   } else {
-      //     const newWard = this.wardRepository.create({
-      //       id: item.id,
-      //       name: item.name,
-      //     });
+        if (existingWard) {
+          await this.wardRepository.update(existingWard.id, {
+            name: item.name,
+          });
+        } else {
+          const newWard = this.wardRepository.create({
+            id: item.id,
+            name: item.name,
+          });
 
-      //     await this.wardRepository.save(newWard);
-      //   }
-      // }
-
+          await this.wardRepository.save(newWard);
+        }
+      }
       this.logger.log('Seeder successfully!');
     } catch (error) {
       this.logger.error('Error seeding data:', error);
