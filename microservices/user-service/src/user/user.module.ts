700a0c65464f1a1be4300d691e1b51acb84ba9cb
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-15 14:21:48 +0700

    update 2 files and delete 3 files

--- microservices/user-service/src/user/user.module.ts
+++ microservices/user-service/src/user/user.module.ts
@@ -13,7 +13,18 @@ import { DatabaseConfig } from './infrastructure/dataaccess/config/database.conf
 @Module({
   imports: [
     TypeOrmModule.forFeature([TaxOffice, Bank, Address]),
-    DatabaseConfig.init(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env['DATABASE_HOST'],
+      port: Number(process.env['DATABASE_PORT']),
+      username: process.env['DATABASE_USERNAME'],
+      password: process.env['DATABASE_PASSWORD'],
+      database: process.env['DATABASE_NAME'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    })
   ],
   controllers: [UserController],
   providers: [UserService, TaxOfficeSeeder, BankSeeder, AddressSeeder],
