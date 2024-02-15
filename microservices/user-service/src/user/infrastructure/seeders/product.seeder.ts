718de5a249576a528e93b980d4b3899d703dccc8
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-15 10:28:08 +0700

    update product.seeder.ts and user.module.ts

--- microservices/user-service/src/user/infrastructure/seeders/product.seeder.ts
+++ microservices/user-service/src/user/infrastructure/seeders/product.seeder.ts
@@ -1,28 +1,16 @@
-
 import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Repository } from 'typeorm';
 
 @Injectable()
-export class  ProductSeeder implements OnModuleInit {
+export class ProductSeeder implements OnModuleInit {
   private logger = new Logger(ProductSeeder.name);
-  
-
-
-
-
-
 
   constructor(
     @InjectRepository(Product)
     private readonly productRepository: Repository<Product>,
   ) {}
-
-
-
-
-
 
   async onModuleInit() {
     // const createProductDto={name:"product1"}
--- microservices/user-service/src/user/user.module.ts
+++ microservices/user-service/src/user/user.module.ts
@@ -5,20 +5,23 @@ import { ProductSeeder } from './infrastructure/seeders/product.seeder';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './infrastructure/seeders/entities/product.entity';
 
-@Module({imports: [TypeOrmModule.forFeature([Product]),
-  TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'mysql',
-    port: 3306,
-    username: 'root',
-    password: 'root_password',
-    database: 'nghia',
-    entities: [__dirname + '/**/*.entity{.ts,.js}'],
-    autoLoadEntities: true,
-    synchronize: true,
-    logging: true,
-  }),],
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'mysql',
+      port: 3306,
+      username: 'root',
+      password: 'root_password',
+      database: 'nghia',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      logging: true,
+    }),
+  ],
   controllers: [UserController],
-  providers: [UserService,ProductSeeder],
+  providers: [UserService, ProductSeeder],
 })
 export class UserModule {}
