aa278c0f86c29ca946372f6a63256b1ebd14f41b
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-16 15:57:54 +0700

    chore: delete bank.ts and bank-id.ts

---
+++ microservices/user-service/src/user/core/domain/entities/bank.ts
@@ -0,0 +1,38 @@
+import { BaseEntity } from '../../../../common/core/domain/entities/base-entity';
+
+
+
+
+
+
+
+
+
+import { BankId } from '../value-objects/bank-id';
+
+
+
+
+
+
+export class Bank    extends BaseEntity<BankId> {
+ 
+ 
+
+  name: string;
+
+  code: string;
+
+  shortName: string;
+
+
+
+
+  
+  constructor(bankId: BankId) {
+    super(bankId);
+  }
+}
+
+
+
