477e5b15d4e92c3e6eea03c42a6e46c50050a47c
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-16 22:59:27 +0700

    update main.ts, main.ts and main.ts

--- microservices/invoice-service/src/main.ts
+++ microservices/invoice-service/src/main.ts
@@ -1,11 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const logger = new Logger();
-  logger.log('Microservice is Running!');
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(
     AppModule,
     {
