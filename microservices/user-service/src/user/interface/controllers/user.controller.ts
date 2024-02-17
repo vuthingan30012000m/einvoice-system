54150d53573d7f6f32424dd638d14a663c24fe24
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-18 00:55:45 +0700

    update user.controller.ts

--- microservices/user-service/src/user/interface/controllers/user.controller.ts
+++ microservices/user-service/src/user/interface/controllers/user.controller.ts
@@ -6,14 +6,17 @@ import {
   Patch,
   Param,
   Delete,
+  UseInterceptors,
 } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CommandBus, QueryBus } from '@nestjs/cqrs';
 
 import { RegisterTaxPayerDto } from '../dto/register/register-tax-payer.dto';
 import { RegisterTaxPayerCommand } from 'src/user/core/application/commands/register-tax-payer/register-tax-payer.command';
+import { RemovePasswordInterceptor } from 'src/user/remove-password.interceptor';
 
 @Controller('user')
+@UseInterceptors(RemovePasswordInterceptor)
 export class UserController {
   constructor(
     private readonly commandBus: CommandBus,
