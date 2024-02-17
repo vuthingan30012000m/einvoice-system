3df748ab2d877ee91725f3f0c12403a6f3a543e7
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-17 08:40:45 +0700

    update bank-detail.ts

--- microservices/user-service/src/user/core/domain/entities/bank-detail.ts
+++ microservices/user-service/src/user/core/domain/entities/bank-detail.ts
@@ -1,10 +1,10 @@
 import { DomainEntity } from '../../../../common/core/domain/entities/domain.entity';
 import { BankDetailId } from '../value-objects/bank-detail-id';
-import { Bank   } from './bank';
+import { Bank } from './bank';
 
 export class BankDetail extends DomainEntity<BankDetailId> {
   accountBank: string;
-  bank: Bank ;
+  bank: Bank;
 
   constructor(bankDetailId: BankDetailId) {
     super(bankDetailId);
