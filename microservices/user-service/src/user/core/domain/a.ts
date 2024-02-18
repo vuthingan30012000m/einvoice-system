386e56771fc064202a7c6e3b7afc2cfba8725d46
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-18 19:35:14 +0700

    update a.ts

--- microservices/user-service/src/user/core/domain/a.ts
+++ microservices/user-service/src/user/core/domain/a.ts
@@ -1,22 +1,20 @@
 import { randomBytes } from 'crypto';
 import { promisify } from 'util';
 import { scrypt } from 'crypto';
+import { Injectable } from '@nestjs/common';
 const scryptAsync = promisify(scrypt);
 
-async function hashPassword(password: string) {
-  const salt = randomBytes(8).toString('hex');
-  const buf = (await scryptAsync(password, salt, 64)) as Buffer;
-  return `${buf.toString('hex')}.${salt}`;
-}
+@Injectable()
+export class HashPasswordService {
+  async hashPassword(password: string) {
+    const salt = randomBytes(8).toString('hex');
+    const buf = (await scryptAsync(password, salt, 64)) as Buffer;
+    return `${buf.toString('hex')}.${salt}`;
+  }
 
-async function comparePasswords(hashedPassword: string, plainPassword: string) {
-  const [hashed, salt] = hashedPassword.split('.');
-  const buf = (await scryptAsync(plainPassword, salt, 64)) as Buffer;
-  return buf.toString('hex') === hashed;
+  async comparePasswords(hashedPassword: string, plainPassword: string) {
+    const [hashed, salt] = hashedPassword.split('.');
+    const buf = (await scryptAsync(plainPassword, salt, 64)) as Buffer;
+    return buf.toString('hex') === hashed;
+  }
 }
-
-(async () => {
-  const plainPassword = 'your_plain_password_here';
-  let hashedPassword = await hashPassword(plainPassword);
-  const isMatch = await comparePasswords(hashedPassword, plainPassword);
-})();
