e053a13983c9ba8305c551f9d4a2650d67cd62fc
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-19 13:09:50 +0700

    update get-tax-payer-current.query-handler.ts

--- microservices/user-service/src/user/core/application/queries/get-tax-payer-current/get-tax-payer-current.query-handler.ts
+++ microservices/user-service/src/user/core/application/queries/get-tax-payer-current/get-tax-payer-current.query-handler.ts
@@ -1,14 +1,41 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
-import {
-  GetTaxPayerCurrentQuery,
-  // GetTaxPayerCurrentQueryResult,
-} from './get-tax-payer-current.query';
+import { GetTaxPayerCurrentQuery } from './get-tax-payer-current.query';
+import { TaxPayerRepositoryPort } from '../../ports/dataaccess/repositories/tax-payer.repository.port';
+import { Logger } from '@nestjs/common';
+import { TaxCode } from 'src/user/core/domain/value-objects/tax-code';
+import { TaxPayerException } from 'src/user/core/domain/exceptions/tax-payer.exception';
 
 @QueryHandler(GetTaxPayerCurrentQuery)
 export class GetTaxPayerCurrentQueryHandler
   implements IQueryHandler<GetTaxPayerCurrentQuery>
 {
-  // constructor(private readonly getTaxPayerCurrentPort: GetTaxPayerCurrentPort) {}
+  private readonly logger = new Logger(GetTaxPayerCurrentQueryHandler.name);
+  
+  
+  
+  
+  
+  
+  constructor(private readonly TaxPayerRepository: TaxPayerRepositoryPort) {}
 
-  public async execute({ payload }: GetTaxPayerCurrentQuery): Promise<void> {}
+  public async execute(payload: GetTaxPayerCurrentQuery) {
+    try {
+      this.logger.debug(
+        `> GetTaxPayerCurrentQuery: ${JSON.stringify(payload)}`,
+      );
+
+      const existingTaxPayer = await this.TaxPayerRepository.getOneById(
+        new TaxCode(payload.taxCode),
+      );
+      console.log('🚀 ~ execute ~ existingTaxPayer:', existingTaxPayer);
+      if (!existingTaxPayer) {
+        throw new TaxPayerException('Không tìm thấy thông tin người nộp thuế.');
+      }
+
+      const { password, usbToken, ...result } = existingTaxPayer;
+      return result;
+    } catch (error) {
+      return { error: error.message };
+    }
+  }
 }
