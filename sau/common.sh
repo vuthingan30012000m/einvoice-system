9307b4d8f46df9c4d4b663c582ca5008ef7a1c87
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-21 03:15:52 +0700

    chore: delete common.sh

---
+++ sau/common.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd /home/vvn20206205/Desktop/einvoice-system/microservices/api-gateway/src && npm i @vuvannghia/common
+cd /home/vvn20206205/Desktop/einvoice-system/microservices/user-service/src && npm i @vuvannghia/common
+cd /home/vvn20206205/Desktop/einvoice-system/microservices/invoice-service/src && npm i @vuvannghia/common
