e812bdc2bae30321a32fb94b019311e470c20bed
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-22 03:15:12 +0700

    update tax-payer-deleted-event.command-handler.ts

--- microservices/invoice-service/src/invoice/core/application/commands/tax-payer-deleted-event/tax-payer-deleted-event.command-handler.ts
+++ microservices/invoice-service/src/invoice/core/application/commands/tax-payer-deleted-event/tax-payer-deleted-event.command-handler.ts
@@ -1,21 +1,43 @@
 import { Logger } from '@nestjs/common';
 import { CommandHandler, ICommandHandler } from '@nestjs/cqrs';
 import { TaxPayerDeletedEventCommand } from './tax-payer-deleted-event.command';
-import { TaxPayerDeletedEventPort } from './tax-payer-deleted-event.port';
+import { TaxCode } from '../../../domain/value-objects/tax-code';
+
+import { TaxPayerRepositoryPort } from '../../ports/dataaccess/repositories/tax-payer.repository.port';
+import { TaxOfficeRepositoryPort } from '../../ports/dataaccess/repositories/tax-office.repository.port';
+import { BankRepositoryPort } from '../../ports/dataaccess/repositories/bank.repository.port';
+import { WardRepositoryPort } from '../../ports/dataaccess/repositories/ward.repository.port';
+import { BankDetailRepositoryPort } from '../../ports/dataaccess/repositories/bank-detail.repository.port';
+import { AddressRepositoryPort } from '../../ports/dataaccess/repositories/address.repository.port';
 
 @CommandHandler(TaxPayerDeletedEventCommand)
 export class TaxPayerDeletedEventCommandHandler
   implements ICommandHandler<TaxPayerDeletedEventCommand>
 {
   constructor(
-    private readonly taxPayerDeletedEventPort: TaxPayerDeletedEventPort,
+    private readonly TaxPayerRepository: TaxPayerRepositoryPort,
+    private readonly TaxOfficeRepository: TaxOfficeRepositoryPort,
+    private readonly BankRepository: BankRepositoryPort,
+    private readonly WardRepository: WardRepositoryPort,
+    private readonly BankDetailRepository: BankDetailRepositoryPort,
+    private readonly AddressRepository: AddressRepositoryPort,
   ) {}
-
   private readonly logger = new Logger(TaxPayerDeletedEventCommandHandler.name);
 
-  public async execute({
-    payload,
-  }: TaxPayerDeletedEventCommand): Promise<void> {
-    this.logger.log(`> TaxPayerDeletedEventCommand: called`);
+  public async execute(payload: TaxPayerDeletedEventCommand) {
+    try {
+      this.logger.log(`> payload: ${JSON.stringify(payload)}`);
+
+      const findTaxPayer = await this.TaxPayerRepository.getOneById(
+        payload.taxCode,
+      );
+
+      findTaxPayer.delete();
+
+      await this.TaxPayerRepository.save(findTaxPayer);
+    } catch (error) {
+      this.logger.error(`> ${error}`);
+      return { message: error.message };
+    }
   }
 }
