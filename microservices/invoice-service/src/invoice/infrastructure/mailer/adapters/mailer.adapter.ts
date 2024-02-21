a439e036ea355d6e7672785d318e70a1c3ae5e7c
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-21 08:48:18 +0700

    1

---
+++ microservices/invoice-service/src/invoice/infrastructure/mailer/adapters/mailer.adapter.ts
@@ -0,0 +1,18 @@
+import { MailerService } from '@nestjs-modules/mailer';
+import { MailerPort } from '../../../core/application/ports/mailer/mailer.port';
+import { Email } from '../../../core/domain/value-objects/email';
+
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export abstract class MailerAdapter implements MailerPort {
+  constructor(private readonly mailerService: MailerService) {}
+
+  async send(receiver: Email, title: string, htmlContent: string) {
+    await this.mailerService.sendMail({
+      to: receiver.value,
+      subject: title,
+      html: htmlContent,
+    });
+  }
+}
