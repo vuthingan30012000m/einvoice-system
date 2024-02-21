e1a2175ed7f0aaac23a407853aa1eef01b2ddd68
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-22 02:57:20 +0700

    update docker-compose.yml and wifi.sh

--- sau/wifi/wifi.sh
+++ sau/wifi/wifi.sh
@@ -3,30 +3,42 @@ echo "Tải offline"
 
 echo "Tải mysql"
 # docker pull mysql
-# docker save -o wifi/image/mysql_image.tar mysql
-docker load -i wifi/image/mysql_image.tar
+# docker save -o sau/wifi/image/mysql_image.tar mysql
+docker load -i sau/wifi/image/mysql_image.tar
 echo "Xong mysql"
 
 echo "Tải node"
 # docker pull node
-# docker save -o wifi/image/node_image.tar node
-docker load -i wifi/image/node_image.tar
+# docker save -o sau/wifi/image/node_image.tar node
+docker load -i sau/wifi/image/node_image.tar
 echo "Xong node"
 
 echo "Tải nats"
 # docker pull nats
-# docker save -o wifi/image/nats_image.tar nats
-docker load -i wifi/image/nats_image.tar
+# docker save -o sau/wifi/image/nats_image.tar nats
+docker load -i sau/wifi/image/nats_image.tar
 echo "Xong nats"
 
 echo "Tải mailhog"
 # docker pull  mailhog/mailhog
-# docker save -o wifi/image/mailhog_image.tar mailhog/mailhog
-docker load -i wifi/image/mailhog_image.tar
+# docker save -o sau/wifi/image/mailhog_image.tar mailhog/mailhog
+docker load -i sau/wifi/image/mailhog_image.tar
 echo "Xong mailhog"
 
 echo "Tải phpmyadmin"
 # docker pull  phpmyadmin/phpmyadmin
-# docker save -o wifi/image/phpmyadmin_image.tar phpmyadmin/phpmyadmin
-docker load -i wifi/image/phpmyadmin_image.tar
+# docker save -o sau/wifi/image/phpmyadmin_image.tar phpmyadmin/phpmyadmin
+docker load -i sau/wifi/image/phpmyadmin_image.tar
 echo "Xong phpmyadmin"
+
+# echo "Tải postgres:alpine"
+# docker pull postgres:alpine
+# docker save -o sau/wifi/image/postgres_image.tar postgres:alpine
+# docker load -i sau/wifi/image/postgres_image.tar
+# echo "Xong postgres"
+
+# echo "Tải pgadmin4"
+# docker pull dpage/pgadmin4
+# docker save -o sau/wifi/image/pgadmin4_image.tar dpage/pgadmin4
+# docker load -i sau/wifi/image/pgadmin4_image.tar
+# echo "Xong pgadmin4"
