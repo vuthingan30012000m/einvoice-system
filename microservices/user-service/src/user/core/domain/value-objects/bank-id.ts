f7291eb806dffa703a5dcd50a3c294c633bc265d
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-21 09:14:44 +0700

    chore: delete bank-id.ts and ward-id.ts

--- microservices/user-service/src/user/core/domain/value-objects/bank-id.ts
+++
@@ -1,7 +0,0 @@
-import { DomainValueObject } from '@vuvannghia/common';
-
-export class BankId extends DomainValueObject {
-  constructor(readonly value: string) {
-    super(value);
-  }
-}
--- microservices/user-service/src/user/core/domain/value-objects/ward-id.ts
+++
@@ -1,7 +0,0 @@
-import { DomainValueObject } from '@vuvannghia/common';
-
-export class WardId extends DomainValueObject {
-  constructor(readonly value: string) {
-    super(value);
-  }
-}
