802f1a06f72125a41939a23a7b886040760eacb6
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-21 09:13:32 +0700

    chore: delete address-id.ts, bank-detail-id.ts and tax-office-id.ts

--- microservices/user-service/src/user/core/domain/value-objects/bank-detail-id.ts
+++
@@ -1,7 +0,0 @@
-import { DomainValueObject } from '@vuvannghia/common';
-
-export class BankDetailId extends DomainValueObject {
-  constructor(readonly value: string) {
-    super(value);
-  }
-}
--- microservices/user-service/src/user/core/domain/value-objects/tax-office-id.ts
+++
@@ -1,7 +0,0 @@
-import { DomainValueObject } from '@vuvannghia/common';
-
-export class TaxOfficeId extends DomainValueObject {
-  constructor(readonly value: string) {
-    super(value);
-  }
-}
